d6ae97b61bd51ee9872f778ca06e895577569ec4
Carllee1113 <ccarllee1113@gmail.com>
2025-03-27 12:20:17 +0800

    Remove GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
